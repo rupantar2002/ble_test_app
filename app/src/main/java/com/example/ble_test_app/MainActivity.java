8e2beb74cec7d8114258e1d0edf067bc89cd8e31
Rupantar <rupantaradhikary2002@gmail.com>
2024-01-01 12:29:27 +0530

    discovering bluetooth devices working

--- app/src/main/java/com/example/ble_test_app/MainActivity.java
+++ app/src/main/java/com/example/ble_test_app/MainActivity.java
@@ -10,6 +10,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -38,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         APP_PERMISSION_REQUEST_CODE_COURSE_LOCATION,
 
         APP_PERMISSION_REQUEST_CODE_FINE_LOCATION,
+
+        APP_PERMISSION_REQUEST_CODE_BACKGROUND_LOCATION,
         APP_PERMISSION_REQUEST_CODE_MAX,
 
     }
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     /*-------UI Elements----*/
     private Button gStartButton = null;
     private Button gScanButton = null;
+    private Button gDiscoveryButton = null;
 
     /*---------------------*/
 
@@ -83,6 +87,69 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     }
                 }
+            }
+        }
+    };
+
+    private final BroadcastReceiver gScanModeChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE");
+                        break;
+                    }
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.SCAN_MODE_CONNECTABLE");
+                        break;
+                    }
+                    case BluetoothAdapter.STATE_CONNECTING: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.STATE_CONNECTING");
+                        break;
+                    }
+                    case BluetoothAdapter.STATE_CONNECTED: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.STATE_CONNECTED");
+                        break;
+                    }
+                    case BluetoothAdapter.STATE_DISCONNECTING: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.STATE_DISCONNECTING");
+                        break;
+                    }
+                    case BluetoothAdapter.STATE_DISCONNECTED: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.STATE_DISCONNECTED");
+                        break;
+                    }
+                    case BluetoothAdapter.SCAN_MODE_NONE: {
+                        Log.d(TAG, "onReceive: action: " + action + " state : BluetoothAdapter.SCAN_MODE_NONE");
+                        break;
+                    }
+                    default: {
+                        // do nothing
+                        break;
+                    }
+                }
+            }
+        }
+    };
+
+    private final BroadcastReceiver gDeviceFoundReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // TODO do something after getting device data
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    Log.e(TAG, "onReceive: permission not  granted i.e" + Manifest.permission.BLUETOOTH_CONNECT);
+                    return;
+                }
+                Log.d(TAG, "onReceive: Device Found { device_name : " + device.getName() + " MAC : " + device.getAddress() + " }");
+                Toast.makeText(MainActivity.this,"{Device Found : device_name : " + device.getName() + " MAC : " + device.getAddress() + " }",Toast.LENGTH_SHORT).show();
             }
         }
     };
@@ -162,6 +229,112 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    private void EnableDisableBluetooth(final Activity activity, final BluetoothAdapter adapter, final BroadcastReceiver receiver) {
+
+        if (adapter == null) {
+            Log.e(TAG, "EnableDisableBluetooth: Bluetooth adapter not found");
+            return;
+        }
+
+        if (adapter.isEnabled() == true) {
+
+            /**
+             * Bluetooth already enabled need to be stopped.
+             */
+            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                Log.e(TAG, "EnableDisableBluetooth: No permission granted");
+                return;
+            }
+
+            try {
+                adapter.disable();
+            } catch (Exception e) {
+                Log.e(TAG, "EnableDisableBluetooth: " + e.getMessage());
+            } finally {
+
+                /**
+                 * Register a Broadcast Receiver for handling bluetooth state change events
+                 */
+                Log.d(TAG, "EnableDisableBluetooth: Bluetooth disable request");
+                IntentFilter disableBtIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+                registerReceiver(receiver, disableBtIntentFilter);
+            }
+
+
+        } else {
+            Log.d(TAG, "EnableDisableBluetooth: Bluetooth enable request");
+
+            /**
+             * Start intent to enable Bluetooth
+             */
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+
+            /**
+             * Register a Broadcast Receiver for handling bluetooth state change events
+             */
+            IntentFilter enableBtIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(receiver, enableBtIntentFilter);
+        }
+
+    }
+
+    private void EnableDisableBluetoothDiscovery(final Activity activity, final BluetoothAdapter adapter, final BroadcastReceiver receiver) {
+
+        if (adapter.isEnabled() == false) {
+            Log.e(TAG, "EnableDisableBluetoothDiscovery: Bluetooth is disabled");
+            Toast.makeText(activity, "Please enable bluetooth", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent enableBtDiscoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        enableBtDiscoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "EnableDisableBluetoothDiscovery: Permission not granted "+ Manifest.permission.BLUETOOTH_ADVERTISE);
+            Toast.makeText(activity, "Enable advertise permission", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        startActivity(enableBtDiscoveryIntent);
+
+        IntentFilter enableBtIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        registerReceiver(receiver, enableBtIntentFilter);
+    }
+
+    private void DiscoverDevices(final Activity activity, final BluetoothAdapter adapter, final BroadcastReceiver receiver) {
+
+        if (adapter.isEnabled() == false) {
+            Log.e(TAG, "DiscoverDevices: Bluetooth is disabled");
+            Toast.makeText(activity, "Please enable bluetooth", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "DiscoverDevices: Permission not granted "+Manifest.permission.BLUETOOTH_SCAN);
+            Toast.makeText(activity, "Enable Scanning permission", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (adapter.isDiscovering() == true) {
+            // TODO check for exceptions
+            adapter.cancelDiscovery();
+            adapter.startDiscovery();
+            IntentFilter startDiscoveryFilter=new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(gDeviceFoundReceiver,startDiscoveryFilter);
+            Log.d(TAG, "DiscoverDevices: start discovery 1");
+
+
+        }else{
+            // TODO check for exceptions
+            adapter.startDiscovery();
+            IntentFilter startDiscoveryFilter=new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(gDeviceFoundReceiver,startDiscoveryFilter);
+            Log.d(TAG, "DiscoverDevices: start discovery 2");
+        }
+
+        Log.d(TAG, "DiscoverDevices: Looking for devices");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -171,6 +344,7 @@ public class MainActivity extends AppCompatActivity {
         /*------------Reference ui elements--------------*/
         gStartButton = (Button) findViewById(R.id.button_start);
         gScanButton = (Button) findViewById(R.id.button_scan);
+        gDiscoveryButton = (Button) findViewById(R.id.button_discover);
         /*-----------------------------------------------*/
 
         /**
@@ -189,6 +363,15 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.BLUETOOTH_CONNECT,
                 "required for this and that");
 
+        RequestPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                AppPermissionRequestCodes.APP_PERMISSION_REQUEST_CODE_BLUETOOTH_ADVERTISE.ordinal(),
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                "required for this and that");
+
+
+
+
         /**
          * Request bluetooth permission.
          */
@@ -200,54 +383,12 @@ public class MainActivity extends AppCompatActivity {
 
         gBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+
         gStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Start button pressed");
-
-                if (gBluetoothAdapter == null) {
-                    Log.e(TAG, "onClick: Bluetooth adapter not found");
-                    return;
-                }
-
-                if (gBluetoothAdapter.isEnabled() == true) {
-                    /**
-                     * Bluetooth already enabled need to be stopped.
-                     */
-
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                        Log.e(TAG, "onClick: No permission granted");
-                        return;
-                    }
-
-                    /**
-                     * Register a Broadcast Receiver for handling bluetooth state change events
-                     */
-                    Log.d(TAG, "onClick: Bluetooth disable request");
-                    IntentFilter disableBtIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-                    registerReceiver(gStateChangeReceiver, disableBtIntentFilter);
-
-                    gBluetoothAdapter.disable();
-
-
-                } else {
-
-                    /**
-                     * Register a Broadcast Receiver for handling bluetooth state change events
-                     */
-                    Log.d(TAG, "onClick: Bluetooth enable request");
-                    IntentFilter enableBtIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-                    registerReceiver(gStateChangeReceiver, enableBtIntentFilter);
-
-                    /**
-                     * Start intent to enable Bluetooth
-                     */
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivity(enableBtIntent);
-
-
-
-                }
+                EnableDisableBluetooth(MainActivity.this,gBluetoothAdapter,gStateChangeReceiver);
             }
         });
 
@@ -255,6 +396,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Scan button pressed");
+                EnableDisableBluetoothDiscovery(MainActivity.this,gBluetoothAdapter,gScanModeChangeReceiver);
+            }
+        });
+
+        gDiscoveryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Discovery button pressed");
+
+                RequestPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        AppPermissionRequestCodes.APP_PERMISSION_REQUEST_CODE_FINE_LOCATION.ordinal(),
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        "required for this and that");
+
+                RequestPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                        AppPermissionRequestCodes.APP_PERMISSION_REQUEST_CODE_BACKGROUND_LOCATION.ordinal(),
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                        "required for this and that");
+
+                DiscoverDevices(MainActivity.this,gBluetoothAdapter,gDeviceFoundReceiver);
+
             }
         });
 
@@ -265,6 +429,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onDestroy: Called");
         super.onDestroy();
         unregisterReceiver(gStateChangeReceiver);
+        unregisterReceiver(gScanModeChangeReceiver);
+        unregisterReceiver(gDeviceFoundReceiver);
     }
 
     @SuppressLint("MissingSuperCall")
